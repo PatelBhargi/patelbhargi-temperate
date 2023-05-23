2f6110afcbd94749989a5f37f12f34cc842755a4
PatelBhargi <133199263+PatelBhargi@users.noreply.github.com>
2023-05-23 12:20:16 +0530

    Add files via upload

---
+++ temperate3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main()
+{
+	int n,last,sum;
+	printf("enter the number:");
+	scanf("%d",&n);
+	last=n%10;
+	q
+	while(n>9)
+	{
+		n=n/10;
+	}
+	sum=n+last;
+	printf("sum of 1st and last digit=%d",sum);
+	return 0;
+}
